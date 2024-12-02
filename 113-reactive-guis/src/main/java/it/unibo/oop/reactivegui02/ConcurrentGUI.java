b467f681e0e95aee23f34a9b351f8767568ab699
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-12-02 19:20:29 +0100

    skeleton of the ConcurrentGUI in the second package completed

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,7 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -41,6 +42,55 @@ public final class ConcurrentGUI extends JFrame {
         stop.addActionListener(event -> agent.stopCounting());
         up.addActionListener(event -> agent.startCountingUp());
         down.addActionListener(event -> agent.startCountingDown());
+    }
+
+    private class Agent implements Runnable {
+        private volatile boolean stop;
+        private boolean up = true;
+        private boolean down;
+        private int counter;
+
+        @Override
+        public void run() {
+                while (this.up && !this.stop) {
+                    try {
+                        final var nextText = Integer.toString(this.counter);
+                        SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                        this.counter++;
+                        Thread.sleep(100);
+                    } catch (InvocationTargetException | InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                while (this.down && !this.stop) {
+                    try {
+                        final var nextText = Integer.toString(this.counter);
+                        SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                        this.counter--;
+                        Thread.sleep(100);
+                    } catch (InvocationTargetException | InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                
+            }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void startCountingUp() {
+            this.up = true;
+            this.down = false;
+        }
+
+        public void startCountingDown() {
+            this.down = true;
+            this.up = false;
+        }
     }
     
 }
